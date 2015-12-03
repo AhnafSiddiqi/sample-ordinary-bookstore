5002ef23cef58be7d7cb790608246b6c9485efff
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-03 18:41:05 +0700

    function authenticates user via password

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+
   def log_in(user)
     session[:login] = user.login_id
   end
@@ -16,4 +17,10 @@ module SessionsHelper
     session.delete(:login)
     @current_user = nil
   end
+
+  def verify_user_password(user, params)
+    user_password = user.password
+    BCrypt::Password.new(params[:session][:password]) == user_password
+  end
+
 end
