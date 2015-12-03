d9bfb352c3201f711b44fa3f523396ef05ba0be2
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-03 12:13:20 +0700

    controller now takes care of basic log-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,20 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create 
+  def create     
+    user = User.find_by(login_id: params[:session][:login].downcase) # need to change to db query
+    if user
+      log_in user
+      render 'new'
+    else 
+      flash.now[:danger] = "Invalid login/password combination"
+      render 'new'
+    end
   end
 
   def destroy
+    log_out
+    render 'new'
   end
 
 end
