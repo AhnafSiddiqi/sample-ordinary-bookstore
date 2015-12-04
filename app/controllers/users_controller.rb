57baf1293a583570c5b4a90dec922f3328ab96b4
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-05 03:26:57 +0700

    sign up now creates a user and a customer simultaneously; it will always create a customer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,37 @@
 class UsersController < ApplicationController 
 
-  def show
-    @user = User.find_by(params[:login_id])
-  end
-
   def new
     @user = User.new
+    render 'signup'
   end
 
   def create
-    @user = User.new(user_params)
-    # if @user.save
-    #   # Handle what happens
-    #   render 'new'
-    # else
-    #   render 'new'
-    # end
-    flash.now[:success] = "You signed up successfully"
-    render 'new'
+    if password_confirmation (params)
+      assign_hashed_password (params)
+      @user = User.new(user_params)
+      if @user.valid?
+        byebug
+        insert_user_db (params)
+        create_customer
+        flash.now[:success] = "You signed up successfully"
+        redirect_to @user
+      else
+        render 'signup'
+      end
+    else 
+      @user = User.new
+      @user.errors.add(:password, "doesn't match!")
+      render 'signup'
+    end
   end
 
   def destroy
   end
 
   private 
+
     def user_params
       params.require(:user).permit(:login_id, :password)
     end
+
 end
