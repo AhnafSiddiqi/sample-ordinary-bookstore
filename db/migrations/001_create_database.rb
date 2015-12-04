35f9cb266612f57eb57fd43c837ebd1d5f48aa0a
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-05 03:34:24 +0700

    changed pk of users, customers, store-managers to integers for easier referencing in routes

--- db/migrations/001_create_database.rb
+++ db/migrations/001_create_database.rb
@@ -15,7 +15,7 @@ class CreateDatabase < ActiveRecord::Migration
       t.text    "authors"
     end
 
-    create_table "customers", primary_key: "login_id", force: :cascade do |t|
+    create_table "customers", force: :cascade do |t|
       t.text    "name"
       t.integer "mobile"
       t.text    "address"
@@ -23,7 +23,7 @@ class CreateDatabase < ActiveRecord::Migration
     end
 
     create_table "orders", force: :cascade do |t|
-      t.text    "customer_id", null: false
+      t.integer "customer_id", null: false
       t.text    "book_id",     null: false
       t.integer "copies"
       t.date    "order_date"
@@ -31,34 +31,38 @@ class CreateDatabase < ActiveRecord::Migration
     end
 
     create_table "review_ratings", id: false, force: :cascade do |t|
-      t.text    "customer_id1", null: false
+      t.integer "customer_id1", null: false
       t.text    "book_id",      null: false
-      t.text    "customer_id2", null: false
+      t.integer "customer_id2", null: false
       t.integer "rating"
     end
 
     create_table "reviews", id: false, force: :cascade do |t|
-      t.text    "customer_id", null: false
+      t.integer "customer_id", null: false
       t.text    "book_id",     null: false
       t.integer "score",       null: false
       t.text    "comment"
     end
 
-    create_table "store_managers", primary_key: "login_id", force: :cascade do |t|
+    create_table "store_managers", force: :cascade do |t|
+      t.text "name"
     end
 
-    create_table "users", primary_key: "login_id", force: :cascade do |t|
+    create_table "users", force: :cascade do |t|
+      t.text "login_id"
       t.text "password", null: false
     end
 
-    add_foreign_key "customers", "users", column: "login_id", primary_key: "login_id", name: "customers_login_id_fkey"
+    add_index "users", ["login_id"], name: "users_login_id_key", unique: true, using: :btree
+
+    add_foreign_key "customers", "users", column: "id", name: "customers_id_fkey", on_delete: :cascade
     add_foreign_key "orders", "books", primary_key: "isbn13", name: "orders_book_id_fkey"
-    add_foreign_key "orders", "customers", primary_key: "login_id", name: "orders_customer_id_fkey"
-    add_foreign_key "review_ratings", "customers", column: "customer_id1", primary_key: "login_id", name: "review_ratings_customer_id1_fkey"
+    add_foreign_key "orders", "customers", name: "orders_customer_id_fkey"
+    add_foreign_key "review_ratings", "customers", column: "customer_id1", name: "review_ratings_customer_id1_fkey"
     add_foreign_key "review_ratings", "reviews", column: "customer_id2", primary_key: "customer_id", name: "review_ratings_customer_id2_fkey"
     add_foreign_key "reviews", "books", primary_key: "isbn13", name: "reviews_book_id_fkey"
-    add_foreign_key "reviews", "customers", primary_key: "login_id", name: "reviews_customer_id_fkey"
-    add_foreign_key "store_managers", "users", column: "login_id", primary_key: "login_id", name: "store_managers_login_id_fkey"
+    add_foreign_key "reviews", "customers", name: "reviews_customer_id_fkey"
+    add_foreign_key "store_managers", "users", column: "id", name: "store_managers_id_fkey"
   end
 
   def self.down
