9a6ab18debfcaf41f0dcbbf40b02e8e415f08e91
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-05 03:30:37 +0700

    model constraint was added so that customers are deleted when users are

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
-  has_many :customers
-  has_many :store_managers
+  has_many :customers, primary_key: 'id', foreign_key: "id", dependent: :delete_all 
+  has_many :store_managers, primary_key: 'id', foreign_key: "id", dependent: :delete_all 
 
 
   before_save { self.login_id = login_id.downcase }
@@ -9,6 +9,5 @@ class User < ActiveRecord::Base
   validates :login_id, presence: true,
                        uniqueness: { case_sensitive: false },
                        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true,
-                       length: { minimum: 4 }
+  validates :password, presence: true
 end
