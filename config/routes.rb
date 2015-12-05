8c2739527fda1d07d9198a39ac09d8f69d2e1e4b
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-05 21:25:07 +0700

    added resources for books

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
   # login routes
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
 
+  # signup routes
   get 'signup'  => 'users#new'
+
 
   resources :users, only: [:show, :destroy]
   resources :users, path: 'signup', only: [:create]
+
+  resources :books, only: [:update, :index, :create]
 end
