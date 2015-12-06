3449e84121ed8615e75017a7d06a1008b4d7de85
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-06 14:29:18 +0700

    new orders are inserted and existing orders are retrieved

--- app/helpers/sql_helper.rb
+++ app/helpers/sql_helper.rb
@@ -13,4 +13,17 @@ module SqlHelper
     Customer.connection.execute(query)
   end
 
+  def insert_order_db(params)
+    customer_id = params[:customer_id]
+    order_date  = params[:order_date]
+    status = params[:status]
+    query = "INSERT INTO orders (customer_id, order_date, status) VALUES ('#{customer_id}','#{order_date}','#{status}')"
+    Order.connection.execute(query)
+  end
+
+  def find_pending_order(user)
+    user_id = user.id
+    order_id = Order.find_by_sql("SELECT id FROM orders WHERE status = 'pending' AND customer_id = '#{user_id}'")
+  end
+
 end
