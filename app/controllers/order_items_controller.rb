4d1f4b7fcb5950713011e3be8eb069aa04c6c522
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-12 11:08:42 +0700

    controller now stores items for orders properly

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,22 +1,43 @@
 class OrderItemsController < ApplicationController 
 
   def create
-
+    params[:items][:isbn13].each do |book|
+      book_index = params[:items][:isbn13].index(book)
+      items = OrderItem.new(order_items_params(book_index))
+      if items.valid?
+        insert_order_items_db(params)
+      end
+    end
+    update_order_status(params[:order_id], "processed")
+    redirect_to books_path
   end
 
   def add
-    puts params
-    isbn = params[:book]
-    store_request isbn
+    book = params[:book]
     puts session[:items]
+    if (copies_available? book)
+      store_for_order book
+    end
+    redirect_to books_path
+  end
+
+  def rmv
+    book = params[:book]
+    remove_from_order book
     redirect_to books_path
   end
 
   private 
 
-    def store_request(isbn)
-      session[:items].nil? ? session[:items] = {} : 0
-      session[:items][isbn].nil? ? session[:items][isbn] = 1 : session[:items][isbn] += 1
+    def order_items_params(index)
+      populate_proper_params(index)
+      params.permit(:order_id, :copies, :book_id)
+    end
+
+    def populate_proper_params(index)
+      params[:order_id] = params[:order_id]
+      params[:copies] = params[:items][:copies][index]
+      params[:book_id] = params[:items][:isbn13][index]
     end
 
 end
