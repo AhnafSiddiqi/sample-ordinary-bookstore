1db2e1c111fcd9b727c6a299fc20fb28e8b82e85
Dhrubajit <dhrubajit1992@gmail.com>
2015-12-12 11:26:50 +0800

    add insertion query for book

--- app/helpers/sql_helper.rb
+++ app/helpers/sql_helper.rb
@@ -13,4 +13,24 @@ module SqlHelper
     Customer.connection.execute(query)
   end
 
+  def insert_book_db(params)
+    isbn13 = params[:book][:isbn13]
+    title = params[:book][:title]
+    format = params[:book][:format]
+    publisher = params[:book][:publisher]
+    year_of_publication = params[:book][:year_of_publication]
+    copies = params[:book][:copies]
+    keywords = params[:book][:keywords]
+    subject = params[:book][:subject]
+    price = params[:book][:price]
+    authors = params[:book][:authors]
+
+    query = "INSERT INTO books (isbn13, title, format, publisher, year_of_publication, copies, keywords, subject, price,
+            authors) VALUES (#{isbn13}, #{title}, #{format}, #{publisher}, #{year_of_publication}, #{copies}, #{keywords},
+            #{subject}, #{price}, #{authors})"
+    Book.connection.execute(query)
+  end
+
 end
+
+
