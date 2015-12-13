028e7531587ccd72a90003873239720ca546bf25
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-13 18:40:09 +0700

    if an order has 0 copies of a book, do not include it in the order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,7 +5,10 @@ class OrderItemsController < ApplicationController
       book_index = params[:items][:isbn13].index(book)
       items = OrderItem.new(order_items_params(book_index))
       if items.valid?
-        insert_order_items_db(params)
+        if (params[:copies] != 0)
+          insert_order_items_db(params)
+          update_book_copies(params)
+        end
       end
     end
     update_order_status(params[:order_id], "processed")
