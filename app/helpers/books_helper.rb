bff3d21aa8840f881654aae0382c89c02b9f1117
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-16 12:54:21 +0700

    removed redundant stuff

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -3,8 +3,7 @@ module BooksHelper
   def copies_available?(isbn)
     if !(session[:items].nil?) && !(session[:items][isbn].nil?)
       max_copies = book_copies_in_store(isbn)
-      puts session[:items][isbn]
-      session[:items][isbn] < max_copies ? true : false
+      session[:items][isbn] <= max_copies ? true : false
     else
       return true
     end
