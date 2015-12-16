db0d3c14b916002f7a581122d5f7e945ed43bd52
AhnafSiddiqi <ahnafsidd@gmail.com>
2015-12-16 18:43:49 +0700

    shows a flash message if no value is selected

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,9 +2,13 @@ class StatisticsController < ApplicationController
 
   def index
     if (params[:m_books].present?)
-      @popular_books = retrieve_m_popular_books(params[:m_books][:value])
-      @popular_authors = retrieve_m_popular_authors(params[:m_books][:value])
-      @popular_publishers = retrieve_m_popular_publishers(params[:m_books][:value])
+      begin
+        @popular_books = retrieve_m_popular_books(params[:m_books][:value])
+        @popular_authors = retrieve_m_popular_authors(params[:m_books][:value])
+        @popular_publishers = retrieve_m_popular_publishers(params[:m_books][:value])
+      rescue
+        flash.now[:danger] = "Please select an appropriate value and try again!"
+      end
     end
     render 'index'
   end
